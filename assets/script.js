9b8c89422d4116c290b2a3970c1f05cae8391e67
Kayleen Carido <kvcarido@gmail.com>
2022-07-18 09:14:23 -0700

    deciding on bg

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,15 @@
-console.log('yerr')
+const getFetch = () => {
+    const url = `http://acnhapi.com/v1a/villagers/56`
+
+    fetch(url)
+    .then(res => res.json()) // parse response as JSON
+    .then(data => {
+        console.log(data)
+        console.log(data.)
+    })
+    .catch(err => {
+        console.log(`error ${err}`)
+    });
+}
+
+getFetch()
