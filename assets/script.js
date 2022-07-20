e100c3963949e47006f3061b509675d5d380d31c
Kayleen Carido <kvcarido@gmail.com>
2022-07-19 21:40:17 -0700

    work on API requests, updating the DOM

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,39 @@
+const rollBtn = document.querySelector("#roll-btn")
+const villagerName = document.querySelector("#villager-name")
+const villagerList = document.querySelector("#villager-list")
+const species = document.querySelector("#stat-species")
+const personality = document.querySelector("#stat-personality")
+const gender = document.querySelector("#stat-gender")
+const birthday = document.querySelector("#stat-birthday")
+const img = document.querySelector("#stat-img")
+
+
+const random = () => {
+    return Math.floor(Math.random() * 390) + 1
+}
+
 const getFetch = () => {
-    const url = `http://acnhapi.com/v1a/villagers/56`
+    let roll = random()
+    const url = `https://acnhapi.com/v1a/villagers/${roll}`
 
     fetch(url)
     .then(res => res.json()) // parse response as JSON
     .then(data => {
-        console.log(data)
-        console.log(data.)
+        villagerName.append(Array(data.name)[0]["name-USen"])
+        species.append(data.species)
+        personality.append(data.personality)
+        gender.append(data.gender)
+        birthday.append(data.birthday)
+        img.src = data.image_uri
     })
     .catch(err => {
         console.log(`error ${err}`)
     });
 }
 
-getFetch()
+
+const updateDom = (name) => {
+    villagerName.innerHTML = name
+}
+
+rollBtn.addEventListener('click', getFetch)
